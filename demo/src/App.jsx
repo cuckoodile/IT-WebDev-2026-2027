2ddcb53c3c923695152b33fe527ef877bd7a23c3
cuckoodile <lhourdeiansube7@gmail.com>
2026-09-25 08:17:21 +0800

    Updated demo

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 export default function App() {
+  useEffect(() => {}, [])
+
+  const [isVisible, setVisible] = useState(false)
+
+  /* useState Syntax
+  > If useState is imported
+  const [getter, setter] = useState(default value: Any)
+
+  > If useState is not import
+  const [getter, setter] = React.useState(default value: Any)
+
+  > The default value of a state can be any type
+    - number (decimal or int)
+    - str
+    - arr (List in python)
+    - obj (Dictionary in python)
+    - bool 
+    - etc...
+
+  */
+
   return (
     <div>App</div>
   )
