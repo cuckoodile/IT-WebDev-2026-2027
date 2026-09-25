fbb8c50b97dbdab32ecc959624666d9d7129236f
cuckoodile <lhourdeiansube7@gmail.com>
2026-09-25 08:35:18 +0800

    Updated demo, added counter display

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from "react";
 
 export default function App() {
-  useEffect(() => {}, [])
-
-  const [isVisible, setVisible] = useState(false)
+  const [count, setCount] = useState(0);
 
   /* useState Syntax
+  useEffect(() => {}, [])
+  
+  const [isVisible, setVisible] = useState(false)
+  
+  > If your are going to use a state that has no default value, use null instead of leaving it empty.
+  // const [userInput, setUserInput] = useState(null)
+
   > If useState is imported
   const [getter, setter] = useState(default value: Any)
 
@@ -24,6 +28,13 @@ export default function App() {
   */
 
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen bg-slate-800 text-white flex flex-col justify-center items-center gap-6 text-5xl">
+      <h1>Counter: {count} </h1>
+
+      <section className="flex gap-4">
+        <button onClick={() => setCount(count + 1)} className="bg-green-600 w-12.5 flex items-center justify-center">+</button>
+        <button onClick={() => setCount(count - 1)} className="bg-red-600 w-12.5 flex items-center justify-center">-</button>
+      </section>
+    </div>
+  );
 }
